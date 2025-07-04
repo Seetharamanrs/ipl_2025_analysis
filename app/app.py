4026ec9682fb5517961dba42ab92f16ce52ac604
seetharamanrs <seetharaman2632001@gmail.com>
2025-07-04 14:12:47 +0100

    Added more Features in App.py

--- app/app.py
+++ app/app.py
@@ -32,8 +32,10 @@ ax2.set_title(f"Top Batsmen in {s_t}")
 ax2.tick_params(axis='x', rotation=30)
 st.pyplot(fig2)
 
+
+
 # Team Score Summary
-st.subheader("Team Total Runs")
+st.header("Team Total Runs")
 
 df["total_run"] = df["runs_of_bat"] + df["extras"]
 team_total = df.groupby("batting_team")["total_run"].sum().sort_values(ascending=False)
@@ -43,3 +45,15 @@ sns.barplot(x=team_total.index, y=team_total.values, ax=ax3)
 ax3.set_title("Total Team Scores in IPL 2025")
 ax3.tick_params(axis='x', rotation=45)
 st.pyplot(fig3)
+
+st.subheader("Max Ball faced by Batsmen overall")
+max_run=df.groupby('striker')['runs_of_bat'].sum()
+max_run=pd.DataFrame(max_run)
+max_run=max_run.sort_values(by='runs_of_bat',ascending=False).head(10)
+fig4,ax4=plt.subplots()
+sns.barplot(x=max_run.index,y=max_run['runs_of_bat'],ax=ax4)
+ax4.set_xlabel("Batsman")
+ax4.tick_params(axis='x',rotation=45)
+ax4.set_ylabel("Runs")
+ax4.set_title("Top run")
+st.pyplot(fig4)
